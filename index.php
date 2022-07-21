42cc241185c29d529505d4e98ae14029f6481aad
Dheerajchandrareddy <48204440+Dheerajchandrareddy@users.noreply.github.com>
2022-07-21 15:04:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
             <li><a href="#">Home</a></li>
             <li>Basic</li>
           </ol>
-          <h1>Welcome to VSSC Team for this session on containers and OpenShift! Thank you for taking out the time to attend this session.</h1>
+          <h1>Welcome to KSITM Team for this session on containers and OpenShift! Thank you for taking out the time to attend this session.</h1>
           <p>The purpose of this application is to demonstrate several interesting features about containers. We hope you enjoy it!</p>
         </div>
       </div>
